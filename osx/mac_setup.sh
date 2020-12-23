6161774c3494b7fdc7e1af34a2902cb4c92224f6
Michael Baldwin <mkbaldwin@users.noreply.github.com>
2020-12-23 10:53:39 -0500

    Slight script tweaks for new OS in preparation for new M1 MBP

--- osx/mac_setup.sh
+++ osx/mac_setup.sh
@@ -5,22 +5,21 @@
 ################################################################################
 
 #Enable unix paths in finder window titles
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+# defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
 
 #Dock customizations. Dock located on the left pinned to the top of the screen
-defaults write com.apple.dock mineffect -string suck
-defaults write com.apple.Dock pinning start
+#defaults write com.apple.Dock pinning start  ## Pinning to the top sadly no longer works
 defaults write com.apple.Dock orientation left
-
-#Enable widgets to be pulled out of the dashboard to the desktop
-defaults write com.apple.dashboard devmode YES
 
 #Default to fully expanded save/open dialog
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool YES
 
+#Enable dark mode
+defaults write /Library/Preferences/.GlobalPreferences AppleInterfaceTheme Dark
+
+#Go back to light more for all UI elements other than the menubar and dock
+defaults write -g NSRequiresAquaSystemAppearance -bool Yes
+
 #Relaunch appropriate system stuff
 killall Dock
 killall Finder
-
-#Enable safari debug mode
-defaults write com.apple.safari IncludeDebugMenu 1
